3a96b87f7c59608fc1c14a41b37b58d8f32178e3
Geert Fokke <geert@grt-dev.com>
2024-01-25 12:05:26 +0100

    style: run new prettier plugin

--- app-ui/src/components/Alert/Alert.tsx
+++ app-ui/src/components/Alert/Alert.tsx
@@ -10,13 +10,13 @@ export const Alert = ({ label, status, description }: AlertProps) => {
     return (
         <div
             role="alert"
-            className={`flex w-full bg-white border-[#E3E3E3] border-2 p-3 gap-3 rounded-lg`}
+            className={`flex w-full gap-3 rounded-lg border-2 border-[#E3E3E3] bg-white p-3`}
         >
-            <div className={`w-6 h-6`}>{icons[status]}</div>
+            <div className={`h-6 w-6`}>{icons[status]}</div>
             <div className={`flex flex-col gap-1`}>
                 <span className={`text-base font-bold text-black`}>{label}</span>
                 {description ? (
-                    <span className={`text-base italic font-normal text-[#535353]`}>
+                    <span className={`text-base font-normal italic text-[#535353]`}>
                         {description}
                     </span>
                 ) : (
