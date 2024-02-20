20f58efd78f54f43b741ef40771d81a28ff40cc9
Geert Fokke <geert@grt-dev.com>
2024-02-20 14:43:45 +0100

    feat: add root import alias path

--- apps/react/vite.config.ts
+++ apps/react/vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), svgr()],
+    resolve: {
+        alias: {
+            '@': path.resolve('/src'),
+        },
+    },
     server: {
         host: true,
         port: 8000,
