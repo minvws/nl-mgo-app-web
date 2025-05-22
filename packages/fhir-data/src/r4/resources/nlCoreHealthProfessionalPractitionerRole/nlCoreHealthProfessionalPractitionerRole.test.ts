625be22b9f635b1289aef885897e0c10a7759293
Rool Paap <rool@rool.nl>
2025-05-22 15:20:47 +0200

    feat: updated nl core health professional practitioner role (#769)

--- packages/fhir-data/src/r4/resources/nlCoreHealthProfessionalPractitionerRole/nlCoreHealthProfessionalPractitionerRole.test.ts
+++ packages/fhir-data/src/r4/resources/nlCoreHealthProfessionalPractitionerRole/nlCoreHealthProfessionalPractitionerRole.test.ts
@@ -1,33 +1,38 @@
 import { expectHealthCareUiSchemaJson, expectJson, testUiSchemaContext } from '$test';
-import { fhirMessage } from '@minvws/mgo-mgo-intl/test';
 import { type PractitionerRole } from 'fhir/r4';
-import { expect, test } from 'vitest';
-import input01 from './fixtures/fhir-resource.json';
+import { test } from 'vitest';
+import input01 from './fixtures/01/fhir-resource.json';
+import input02 from './fixtures/02/fhir-resource.json';
 import { nlCoreHealthProfessionalPractitionerRole } from './nlCoreHealthProfessionalPractitionerRole';
-import { i18n } from './uiSchema';
 
-test('returns the expected output 01', async () => {
+test('01: mgo-resource', async () => {
     const output = nlCoreHealthProfessionalPractitionerRole.parse(input01 as PractitionerRole);
-    await expectJson(output).toMatchFileSnapshot('./fixtures/mgo-resource.snap.json');
+    await expectJson(output).toMatchFileSnapshot('./fixtures/01/mgo-resource.snap.json');
 });
 
-test('uiSchema returns the expected output 01', async () => {
+test('01: ui-schema', async () => {
     const output = nlCoreHealthProfessionalPractitionerRole.parse(input01 as PractitionerRole);
     const uiSchema = nlCoreHealthProfessionalPractitionerRole.uiSchema(
         output,
         testUiSchemaContext()
     );
     await expectHealthCareUiSchemaJson(uiSchema).toMatchFileSnapshot(
-        './fixtures/ui-schema.snap.json'
+        './fixtures/01/ui-schema.snap.json'
     );
 });
 
-test('uiSchema label returns profile when label not specified', () => {
-    const output = nlCoreHealthProfessionalPractitionerRole.parse(input01 as PractitionerRole);
-    output.speciality = undefined;
+test('02: mgo-resource', async () => {
+    const output = nlCoreHealthProfessionalPractitionerRole.parse(input02 as PractitionerRole);
+    await expectJson(output).toMatchFileSnapshot('./fixtures/02/mgo-resource.snap.json');
+});
+
+test('02: ui-schema', async () => {
+    const output = nlCoreHealthProfessionalPractitionerRole.parse(input02 as PractitionerRole);
     const uiSchema = nlCoreHealthProfessionalPractitionerRole.uiSchema(
         output,
         testUiSchemaContext()
     );
-    expect(uiSchema.label).toEqual(fhirMessage(i18n));
+    await expectHealthCareUiSchemaJson(uiSchema).toMatchFileSnapshot(
+        './fixtures/02/ui-schema.snap.json'
+    );
 });
--- packages/fhir-data/src/r4/resources/nlCoreHealthProfessionalPractitionerRole/nlCoreHealthProfessionalPractitionerRole.ts
+++ packages/fhir-data/src/r4/resources/nlCoreHealthProfessionalPractitionerRole/nlCoreHealthProfessionalPractitionerRole.ts
@@ -2,24 +2,27 @@ import { FhirVersion } from '@minvws/mgo-fhir-types';
 import { type PractitionerRole } from 'fhir/r4';
 import { parse } from '../../../parse';
 import { type ResourceConfig } from '../../../types';
+import { generateUiSchema } from '../../../ui/generator';
 import { map } from '../../../utils';
 import { parseNlCoreContactInformation } from '../../elements';
-import { uiSchema } from './uiSchema';
 
 const profile =
     'http://nictiz.nl/fhir/StructureDefinition/nl-core-HealthProfessional-PractitionerRole'; // NOSONAR
 
 /**
- * @see: https://simplifier.net/packages/nictiz.fhir.nl.r4.nl-core/0.11.0-beta.1/files/2628465
+ * @see: https://simplifier.net/packages/nictiz.fhir.nl.r4.nl-core/0.11.0-beta.1/files/2628465 (nl core HealthProfessional PractitionerRole)
+ * @see: https://simplifier.net/packages/nictiz.fhir.nl.r4.zib2020/0.11.0-beta.1/files/2628201 (zib HealthProfessional PractitionerRole)
  */
 function parseNlCoreHealthProfessionalPractitionerRole(resource: PractitionerRole) {
     return {
         ...parse.resourceMeta(resource, profile, FhirVersion.R4),
-        practitioner: parse.reference(resource.practitioner),
-        organization: parse.reference(resource.organization),
-        location: map(resource.location, parse.reference),
-        speciality: map(resource.specialty, parse.codeableConcept),
+
+        // zib ContactInformation-v1.2(2020EN)
         telecom: parseNlCoreContactInformation(resource.telecom),
+
+        // zib HealthProfessional-v3.5(2020EN)
+        organization: parse.reference(resource.organization),
+        speciality: map(resource.specialty, parse.codeableConcept),
     };
 }
 
@@ -30,5 +33,5 @@ export type R4NlCoreHealthProfessionalPractitionerRole = ReturnType<
 export const nlCoreHealthProfessionalPractitionerRole = {
     profile,
     parse: parseNlCoreHealthProfessionalPractitionerRole,
-    uiSchema,
+    uiSchema: generateUiSchema,
 } satisfies ResourceConfig<PractitionerRole, R4NlCoreHealthProfessionalPractitionerRole>;
