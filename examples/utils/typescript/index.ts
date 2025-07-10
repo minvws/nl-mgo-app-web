6d0ee193db0298d5a6cb650f0a0720c2f736a385
Geert Fokke <geert@grt-dev.com>
2025-07-10 16:44:47 +0200

    test: update example test scripts

--- examples/utils/typescript/index.ts
+++ examples/utils/typescript/index.ts
@@ -1,10 +1,14 @@
 import { isNullish } from '@minvws/mgo-utils';
+import packageJson from '@minvws/mgo-utils/package.json' with { type: 'json' };
+
+const { name, version } = packageJson;
 
 const expected = true;
 const result = isNullish(undefined);
 
 if (result !== expected) {
-    throw new Error(`❌ Test failed: expected "${JSON.stringify(expected)}", got "${result}"`);
+    console.log(`❌ ${name}@${version} - Test failed`);
+    throw new Error(`expected "${JSON.stringify(expected)}", got "${result}"`);
 }
 
-console.log('✅ Test passed');
+console.log(`✅ ${name}@${version} - Test passed`);
