7d2cde3b7341c086285d0f645bb6ff1a02b60415
Geert Fokke <geert@grt-dev.com>
2025-07-24 09:15:26 +0200

    chore: update fhir examples to ensure r3 and r4 types are exported

--- examples/fhir/typescript/index.ts
+++ examples/fhir/typescript/index.ts
@@ -1,5 +1,7 @@
 import { isFhirResource, type ResourceByType } from '@minvws/mgo-fhir';
 import packageJson from '@minvws/mgo-fhir/package.json' with { type: 'json' };
+import { type Patient as PatientR3 } from '@minvws/mgo-fhir/r3';
+import { type Patient as PatientR4 } from '@minvws/mgo-fhir/r4';
 
 const { name, version } = packageJson;
 
@@ -7,11 +9,15 @@ const expected = true;
 let result = false;
 
 const unknown = { resourceType: 'Patient' };
-const patient = {} as ResourceByType<'Patient'>;
+const patientRx = {} as ResourceByType<'Patient'>;
+const patientR3 = {} as PatientR3;
+const patientR4 = {} as PatientR4;
 
 if (isFhirResource(unknown, 'Patient')) {
     unknown.birthDate = '2025-01-01';
-    patient.birthDate = '2025-01-01';
+    patientRx.birthDate = '2025-01-01';
+    patientR3.birthDate = '2025-01-01';
+    patientR4.birthDate = '2025-01-01';
     result = true;
 }
 
