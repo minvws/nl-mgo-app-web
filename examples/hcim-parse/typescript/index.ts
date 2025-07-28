3fcd675ba039aac1babc75ae4e544bc66110c59f
Geert Fokke <geert@grt-dev.com>
2025-07-28 09:45:57 +0200

    chore: update hcim-parse example test

--- examples/hcim-parse/typescript/index.ts
+++ examples/hcim-parse/typescript/index.ts
@@ -1,14 +1,17 @@
-import { isNullish } from '@minvws/mgo-hcim-parse';
+import { parse } from '@minvws/mgo-hcim-parse';
 import packageJson from '@minvws/mgo-hcim-parse/package.json' with { type: 'json' };
 
 const { name, version } = packageJson;
 
-const expected = true;
-const result = isNullish(undefined);
+const expected = {
+    _type: 'code',
+    value: 'foobar',
+};
+const result = parse.code('foobar');
 
-if (result !== expected) {
+if (JSON.stringify(result) !== JSON.stringify(expected)) {
     console.log(`❌ ${name}@${version} - Test failed`);
-    throw new Error(`expected "${JSON.stringify(expected)}", got "${result}"`);
+    throw new Error(`expected "${JSON.stringify(expected)}", got "${JSON.stringify(result)}"`);
 }
 
 console.log(`✅ ${name}@${version} - Test passed`);
