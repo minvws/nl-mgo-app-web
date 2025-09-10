fe2c47938201f485cef0085bed129a3cd0691a66
Otto Boekesteijn <otto@boekesteijn.nl>
2025-09-10 16:27:43 +0200

    test: update Description test

--- packages/ui/src/components/DescriptionItem/DescriptionItem.test.tsx
+++ packages/ui/src/components/DescriptionItem/DescriptionItem.test.tsx
@@ -1,28 +1,36 @@
 import { faker } from '@faker-js/faker';
-import { render } from '@testing-library/react';
-import { test } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { expect, test } from 'vitest';
 import { DescriptionItem, type DescriptionItemProps } from './DescriptionItem';
 
 test('render item', () => {
-    const props: DescriptionItemProps = {
+    const props = {
         term: faker.word.sample(),
         details: faker.word.sample(),
-    };
+    } satisfies DescriptionItemProps;
+
     render(<DescriptionItem {...props} />);
+    const definition = screen.getByRole('definition', { name: props.term });
+    expect(definition).toHaveTextContent(props.details);
 });
 
 test('render item with undefined term', () => {
-    const props: DescriptionItemProps = {
+    const props = {
         term: undefined,
         details: faker.word.sample(),
-    };
+    } satisfies DescriptionItemProps;
+
     render(<DescriptionItem {...props} />);
+    const element = screen.getByText(props.details);
+    expect(element).toBeVisible();
 });
 
 test('render item with undefined details', () => {
-    const props: DescriptionItemProps = {
+    const props = {
         term: faker.word.sample(),
         details: undefined,
-    };
+    } satisfies DescriptionItemProps;
     render(<DescriptionItem {...props} />);
+    const definition = screen.getByRole('definition', { name: props.term });
+    expect(definition).toHaveTextContent('');
 });
