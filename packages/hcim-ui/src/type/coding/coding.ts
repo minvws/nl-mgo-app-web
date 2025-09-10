c0231043d88cd11a23b3fb2ae6a164516d934058
Otto Boekesteijn <otto@boekesteijn.nl>
2025-09-10 16:29:27 +0200

    feat: add MgoCoding details to ui schema value #841

--- packages/hcim-ui/src/type/coding/coding.ts
+++ packages/hcim-ui/src/type/coding/coding.ts
@@ -1,12 +1,23 @@
-import { type MgoCoding } from '@minvws/mgo-hcim-parse';
-import { isNonNullish } from '@minvws/mgo-utils';
+import { MgoCodingProps, type MgoCoding } from '@minvws/mgo-hcim-parse';
+import { isNonNullish, Nullable } from '@minvws/mgo-utils';
+import { UiContext } from 'src/context/index.js';
 import { system } from '../../format/system/system.js';
 import {
+    type DisplayCoding,
     type MultipleValues,
     type SingleValue,
     type UiFunction,
     type WithUiContext,
 } from '../../types/index.js';
+
+export const codingToDisplay = (uiContext: UiContext) => {
+    const display = system(uiContext);
+    return (value: Nullable<MgoCodingProps>): DisplayCoding => ({
+        display: display(value),
+        code: value?.code,
+        system: value?.system,
+    });
+};
 
 export const coding: WithUiContext<
     UiFunction<MgoCoding | MgoCoding[], SingleValue | MultipleValues>
@@ -14,18 +25,18 @@ export const coding: WithUiContext<
     (context) =>
     (label, value, options = {}) => {
         const { formatLabel } = context;
-        const display = system(context);
+        const formatCoding = codingToDisplay(context);
 
         if (Array.isArray(value)) {
             return {
                 label: formatLabel(label, value, options.defaultLabel),
                 type: 'MULTIPLE_VALUES',
-                display: value.map(display).filter(isNonNullish),
+                display: value.map(formatCoding).filter((item) => isNonNullish(item.display)),
             };
         }
         return {
             label: formatLabel(label, value, options.defaultLabel),
             type: 'SINGLE_VALUE',
-            display: display(value),
+            display: formatCoding(value),
         };
     };
