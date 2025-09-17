902778b202c74bac59782c929733baf9a26e1c3b
Geert Fokke <geert@grt-dev.com>
2025-09-17 11:02:16 +0200

    feat(config): add health category icons and update data service configs

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -3,13 +3,20 @@ import commonClinicalDataset from './data-services/48-common-clinical-dataset.js
 import generalPractitionerData from './data-services/49-general-practitioner-data.json' with { type: 'json' };
 import documentsPdfa from './data-services/51-documents-pdfa.json' with { type: 'json' };
 import vaccinationImmunization from './data-services/63-vaccination-immunization.json' with { type: 'json' };
-import healthCategories from './health-categories.json' with { type: 'json' };
+import healthCategoriesConfig from './health-categories.json' with { type: 'json' };
 
-const dataServices = {
+const dataServicesConfig = {
     commonClinicalDataset,
     documentsPdfa,
     generalPractitionerData,
     vaccinationImmunization,
 };
 
-export { dataServices, healthCategories };
+export { dataServicesConfig, healthCategoriesConfig };
+
+export type MainHealthCategoryConfig = (typeof healthCategoriesConfig)[number];
+export type HealthCategoryConfig = MainHealthCategoryConfig['categories'][number];
+export type SubHealthCategoryConfig = HealthCategoryConfig['subcategories'][number];
+
+export type DataServiceConfig = (typeof dataServicesConfig)[keyof typeof dataServicesConfig];
+export type DataServiceEndpointConfig = DataServiceConfig['endpoints'][number];
