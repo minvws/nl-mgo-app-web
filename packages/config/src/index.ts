c67be5d2e2a20e6339ac34bb5e9479925564c6c7
Rool Paap <rool@rool.nl>
2025-10-22 10:41:51 +0200

    feat(mgo-939): added data service for GGZ (#956)

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -1,6 +1,7 @@
 /* c8 ignore start */
 import commonClinicalDataset from './data-services/48-common-clinical-dataset.json' with { type: 'json' };
 import generalPractitionerData from './data-services/49-general-practitioner-data.json' with { type: 'json' };
+import mentalHealthCareInformation from './data-services/50-mental-health-care-information.json' with { type: 'json' };
 import documentsPdfa from './data-services/51-documents-pdfa.json' with { type: 'json' };
 import longTermHealthcare from './data-services/61-long-term-healthcare-information.json' with { type: 'json' };
 import vaccinationImmunization from './data-services/63-vaccination-immunization.json' with { type: 'json' };
@@ -12,6 +13,7 @@ const dataServicesConfig = {
     generalPractitionerData,
     vaccinationImmunization,
     longTermHealthcare,
+    mentalHealthCareInformation,
 };
 
 export { dataServicesConfig, healthCategoriesConfig };
