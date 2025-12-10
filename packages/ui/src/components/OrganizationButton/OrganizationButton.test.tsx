fc951bf3c8f90fa97b100815d946c46c24533c09
Geert Fokke <geert@grt-dev.com>
2025-12-10 09:35:26 +0100

    chore(ui): improve code coverage

--- packages/ui/src/components/OrganizationButton/OrganizationButton.test.tsx
+++ packages/ui/src/components/OrganizationButton/OrganizationButton.test.tsx
@@ -15,6 +15,32 @@ test('renders ButtonCard', async () => {
     expect(element).toBeVisible();
 });
 
+test('renders ButtonCard with infoMessage', async () => {
+    const props: OrganizationButtonProps = {
+        title: faker.word.sample(),
+        subTitle: faker.word.sample(),
+        infoMessage: faker.word.sample(),
+    };
+
+    render(<OrganizationButton {...props} />);
+
+    const element = await screen.findByRole('button');
+    expect(element).toHaveTextContent(props.infoMessage as string);
+});
+
+test('renders ButtonCard with successMessage', async () => {
+    const props: OrganizationButtonProps = {
+        title: faker.word.sample(),
+        subTitle: faker.word.sample(),
+        successMessage: faker.word.sample(),
+    };
+
+    render(<OrganizationButton {...props} />);
+
+    const element = await screen.findByRole('button');
+    expect(element).toHaveTextContent(props.successMessage as string);
+});
+
 test('is aria-disabled and ignores clicks when disabled', async () => {
     const onClick = vi.fn();
     const props: OrganizationButtonProps = {
