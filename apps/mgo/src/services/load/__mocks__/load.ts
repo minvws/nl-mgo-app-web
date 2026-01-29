4eed756e8f7e0e8d6f8fb1d59fb2b13c3a670a45
Geert Fokke <geert@grt-dev.com>
2026-01-29 12:31:09 +0100

    chore(mgo): fix type

--- apps/mgo/src/services/load/__mocks__/load.ts
+++ apps/mgo/src/services/load/__mocks__/load.ts
@@ -1,9 +1,10 @@
 import { vi } from 'vitest';
+import { LoadService } from '../load';
 
 const loadService = {
     search: vi.fn(),
 };
 
-export function getLoadService() {
+export function getLoadService(): LoadService {
     return loadService;
 }
